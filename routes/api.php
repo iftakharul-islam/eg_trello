1ec09b649d5a925d1cfc3fce2ebdcf1c74cf46d2
iftakharul-islam <iftakharulislam881@gmail.com>
2022-07-20 09:01:04 +0600

    Passport installed

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Str;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+ 
+Route::get('/redirect', function (Request $request) {
+    $request->session()->put('state', $state = Str::random(40));
+ 
+    $query = http_build_query([
+        'client_id' => '1',
+        'redirect_uri' => 'http://third-party-app.com/callback',
+        'response_type' => '200',
+        'scope' => '',
+        'state' => $state,
+    ]);
+ 
+    return redirect('http://127.0.0.1:8000/api/oauth/authorize?'.$query);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
